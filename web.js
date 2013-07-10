63c15220acde6ac87c30ec53f759d474235a106f
Ubuntu <ubuntu@ip-10-254-115-36.us-west-2.compute.internal>
2013-07-10 02:25:01 +0000

    modified web.js after error

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var express = require('express');
-var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-
+var fs = require('fs');
 var buffer = new buffer ();
   response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))
 });
